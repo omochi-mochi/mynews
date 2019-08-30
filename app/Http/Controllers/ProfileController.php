cf8c120276dabb7470f613c247fbcc01ead98737
まい <harusame79@hotmail.co.jp>
2019-08-30 11:54:30 +0000

    first commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,6 +14,7 @@ public function index(Request $request)
     $profile = Profile::where('user_id',$request->user()->id)->first;
     if(empty($profile)) {
         abort(404);
+        
     }
     
     return view('profile.index', ['profile' => $profile]);
